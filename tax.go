bcf81af0f8683678f752de7de0248ecff37161de
Marcus Assia <assia.marcus@gmail.com>
2020-12-11 08:55:28 -0500

    changes to export types

--- tax.go
+++ tax.go
@@ -5,7 +5,7 @@ import(
 	"math"
 )
 
-type taxpayer struct {
+type Taxpayer struct {
 	Name   string
 	Income float64
   Year int
@@ -13,24 +13,24 @@ type taxpayer struct {
   States []string
 }
 
-type bracket struct {
+type Bracket struct {
 	Status    string
 	Threshold []float64
 }
 
-type taxes interface {
+type Taxes interface {
 	fedTax() float64
 }
 
-func calc(t taxes) {
+func Calc(t taxes) {
 	fmt.Println(t.fedTax())
 }
 
-func (tp taxpayer) fedTax() float64 {
+func (tp Taxpayer) fedTax() float64 {
 	r := make(map[int][]float64)
 	r[2020] = []float64{.1, .12, .22, .24, .32, .35, .37}
-	br := make(map[int][]bracket)
-	br[2020] = []bracket{
+	br := make(map[int][]Bracket)
+	br[2020] = []Bracket{
 		{"IND", []float64{0, 9875, 40125, 85525, 163300, 207350, 518400}},
 		{"MFJ", []float64{0, 19750, 80250, 171050, 326600, 414700, 622050}},
 	}
